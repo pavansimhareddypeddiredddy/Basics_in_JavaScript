917abdc16ea4c3a0efbe89a0f06e4bee63d630bc
pavan simha reddy <119424574+pavansimhareddypeddiredddy@users.noreply.github.com>
2025-07-05 05:06:49 +0000

    05-07-2025, 10:36

---
+++ 01_Basics/Datatypes_03.js
@@ -0,0 +1,17 @@
+// data types are the type of data that we are storing in variables.
+// types in data types 
+//  number : inclues both decimal and integers
+//  bigint : usally for the big numbers
+//  string : charaters , words and lines comes in string under
+//  boolean: just true or false
+//   null : intentionally keeping and empty value in a variable  // type of null is object as per bitpattern(000) same for object and null
+//    undefined : just creating a variable without assinging value comes under undefined
+//     symbol: for uniqueness even values same shows not equal.
+//  typeof is used to check the variable or value in which data type
+let name = "pavan simha reddy";  // string
+let age = 21;  // number
+let maritialStatus = false;  //boolean
+let weight;                    // undefined 
+let salary = null;            //null
+
+
